56c05250143487562095990d2ddb2835ef69cd35
Valantis Kamayiannis <v.kamayiannis@orosimosoftware.gr>
2017-04-03 16:17:47 +0300

    change window alignment

--- js/background.js
+++ js/background.js
@@ -2,7 +2,7 @@ chrome.app.runtime.onLaunched.addListener(function () {
   chrome.app.window.create('window.html', {
     'outerBounds': {
       'width': 1050,
-      'height': 500
+      'height': 800
     }
   });
 });
